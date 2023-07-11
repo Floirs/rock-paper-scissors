25dd4e059497ef0023b70980478775d23432e1ca
Floirs Heiz <98380831+Floirs@users.noreply.github.com>
2023-07-10 21:01:49 -0300

    add ui to rps

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let winningMoves = {
-    "rock": "scissors",
-    "paper": "rock",
-    "scissors": "paper",
+    "Rock": "Scissors",
+    "Paper": "Rock",
+    "Scissors": "Paper",
 }
 
+let playerScore = 0;
+let enemyScore = 0;
+
 function getComputerChoice() {
-    let computerOptions = ["rock", "paper", "scissors"];
+    let computerOptions = ["Rock", "Paper", "Scissors"];
     let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
     return computerChoice;
 }
@@ -15,31 +18,45 @@ function playRound(playerSelection, computerSelection) {
     // console.log("Computer played:", computerSelection)
     if (playerSelection in winningMoves === false) {throw new Error("Not a valid hand")};
     if (winningMoves[playerSelection] == computerSelection) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return "win";
+        // console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        // return `You win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win!`;
     } else if (playerSelection == computerSelection) {
         console.log("Draw")
-        return "draw";
+        return "It's a Draw";
     }
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    return false;
+    // console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    enemyScore++;
+    // return `You lose! ${computerSelection} beats ${playerSelection}`;
+    return `You Lose!`;
 
 }
 
 function game() {
-    let playerScore = 0;
-    let drawCount = 0;
-    for (let i = 0; i < 5; i++) {
-        playerHand = prompt("Choose your hand:").toLowerCase();
-        roundResult = playRound(playerHand, getComputerChoice());
-        if (roundResult == "win") {
-            ++playerScore;
-        } else if (roundResult == "draw") {
-            ++drawCount;
-        }
-        console.log("End of round", i + 1);
-    }
-    console.log(`Player final score: ${playerScore}\nComputer final score: ${5 - playerScore - drawCount}`);
+    const btns = document.querySelectorAll("button");
+    const result = document.querySelector(".result");
+    const plrScore = document.querySelector(".score-player");
+    const enmScore = document.querySelector(".score-enemy")
+
+    btns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            // console.log(e.target.className)
+            if (e.target.className == "reset") {
+                playerScore = 0;
+                enemyScore = 0;
+                plrScore.innerText = `Player Score: ${playerScore}`;
+                enmScore.innerText = `Enemy Score: ${enemyScore}`;
+                result.innerText = "Press the buttons above to play";
+                return;
+            }
+            const roundResult = playRound(e.target.className, getComputerChoice())
+            result.innerText = roundResult
+            plrScore.innerText = `Player Score: ${playerScore}`;
+            enmScore.innerText = `Enemy Score: ${enemyScore}`;
+        })
+    })
 }
+
 
 game();
